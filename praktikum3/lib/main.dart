fcf0c7417df463b9e78a53318bc371ff60fbce08
septyaanp <septyaandiniputri@gmail.com>
2024-03-15 10:32:46 +0700

    step 2 : add Widget Body & widget text

--- praktikum3/lib/main.dart
+++ praktikum3/lib/main.dart
@@ -7,26 +7,20 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Aplikasi Pertemuan 3'),
-          backgroundColor: Color.fromARGB(
-              255, 81, 156, 248), // Ubah warna latar belakang AppBar
+          title: Text('Hello World'),
+          backgroundColor: Color.fromARGB(255, 81, 156, 248),
         ),
-        body: Container(
-          margin: EdgeInsets.only(top: 100, left: 50),
-          color: Colors.amber,
-          child: Text('Hello World',
-              style: TextStyle(
-                color: Colors.blueAccent,
-                fontSize: 50,
-              )
-              // Ubah warna teks
-              ),
+        body: Text(
+          'Hello World',
+          style: TextStyle(
+            color: Colors.blueAccent,
+            fontSize: 50,
+          ),
         ),
       ),
     );
