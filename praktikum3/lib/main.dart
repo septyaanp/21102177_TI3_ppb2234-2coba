a0c18e9a6d29f146ef77679f734df0f6dbee586a
septyaanp <septyaandiniputri@gmail.com>
2024-03-15 11:02:10 +0700

    step 13 : update home_screen.dart

--- praktikum3/lib/main.dart
+++ praktikum3/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:praktikum3/bloc/contact_cubit.dart';
+import 'package:praktikum3/views/home_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -8,6 +9,7 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MultiBlocProvider(
