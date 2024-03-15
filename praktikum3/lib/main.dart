379c0cb5733b451aafa6ca07b46ce2f0bd5d3ea4
septyaanp <septyaandiniputri@gmail.com>
2024-03-15 10:41:23 +0700

    step 6 : update main.dart

--- praktikum3/lib/main.dart
+++ praktikum3/lib/main.dart
@@ -5,30 +5,11 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
+  const MyApp({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Aplikasi Pertemuan 3'),
-          backgroundColor: Color.fromARGB(
-              255, 81, 156, 248), // Ubah warna latar belakang AppBar
-        ),
-        body: Container(
-          margin: EdgeInsets.only(top: 100, left: 50),
-          color: Colors.amber,
-          child: Text('Hello World',
-              style: TextStyle(
-                color: Colors.blueAccent,
-                fontSize: 50,
-              )
-              // Ubah warna teks
-              ),
-        ),
-      ),
+    return const MaterialApp(
+      home: HomeScreen(),
     );
   }
 }
