4199a02e7aba32728c52e61ae97f60474bdb2ce8
septyaanp <septyaandiniputri@gmail.com>
2024-03-21 13:10:10 +0700

    step 3 : update main.dart

--- praktikum5/lib/main.dart
+++ praktikum5/lib/main.dart
@@ -8,6 +8,9 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp();
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    );
   }
 }
