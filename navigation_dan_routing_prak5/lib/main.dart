3960245d157a44ca5ba14c71624f6aaad178308f
septyaanp <septyaandiniputri@gmail.com>
2024-03-22 08:12:29 +0700

    step 12 : emndaftarkan Route tiap halaman

--- navigation_dan_routing_prak5/lib/main.dart
+++ navigation_dan_routing_prak5/lib/main.dart
@@ -1,5 +1,8 @@
+import 'dart:js';
+
 import 'package:flutter/material.dart';
 import 'package:navigation_dan_routing_prak5/home_page.dart';
+import 'package:navigation_dan_routing_prak5/second_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,6 +16,11 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowMaterialGrid: false,
+      initialRoute: '/',
+      routes: {
+        '/': (context) => const HomePage(),
+        '/second_page': (context) => const SecondPage(),
+      },
       home: HomePage(),
     );
   }
