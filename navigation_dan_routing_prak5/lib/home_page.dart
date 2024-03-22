4f5734843fd473a13c96b96d87ba8157ad9781f8
septyaanp <septyaandiniputri@gmail.com>
2024-03-22 07:59:05 +0700

    step 7 : menambahkan navigator

--- navigation_dan_routing_prak5/lib/home_page.dart
+++ navigation_dan_routing_prak5/lib/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:navigation_dan_routing_prak5/second_page.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -12,7 +13,10 @@ class HomePage extends StatelessWidget {
       ),
       body: Center(
         child: ElevatedButton(
-          onPressed: () {},
+          onPressed: () {
+            Navigator.push(context,
+                MaterialPageRoute(builder: (context) => const SecondPage()));
+          },
           child: const Text('menuju halaman kedua'),
         ),
       ),
