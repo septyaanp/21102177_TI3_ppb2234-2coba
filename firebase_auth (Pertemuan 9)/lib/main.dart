59cd52bac815721d2cf0484f1e90f5b817fcc188
septyaanp <septyaandiniputri@gmail.com>
2024-05-03 12:44:53 +0700

    step 9 : update main.dart

--- firebase_auth (Pertemuan 9)/lib/main.dart
+++ firebase_auth (Pertemuan 9)/lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:pertemuan9/firebase_options.dart';
 
+import 'bloc/login/login_cubit.dart';
+import 'bloc/register/register_cubit.dart';
 import 'ui/splash.dart';
 import 'utils/routes.dart';
 
@@ -18,12 +21,18 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: "Praktikum 9",
-      debugShowCheckedModeBanner: false,
-      navigatorKey: NAV_KEY,
-      onGenerateRoute: generateRoute,
-      home: SplashScreen(),
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(create: (context) => LoginCubit()),
+        BlocProvider(create: (context) => RegisterCubit())
+      ],
+      child: MaterialApp(
+        title: "Praktikum 9",
+        debugShowCheckedModeBanner: false,
+        navigatorKey: NAV_KEY,
+        onGenerateRoute: generateRoute,
+        home: SplashScreen(),
+      ),
     );
   }
 }
