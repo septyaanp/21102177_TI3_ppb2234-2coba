546ee449185985ea72064f1ab7431b31596b40e4
septyaanp <septyaandiniputri@gmail.com>
2024-05-18 20:29:32 +0700

    step 16 : update main.dart

--- firebase_auth (Pertemuan 9)/lib/main.dart
+++ firebase_auth (Pertemuan 9)/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -5,6 +6,8 @@ import 'package:pertemuan9/firebase_options.dart';
 
 import 'bloc/login/login_cubit.dart';
 import 'bloc/register/register_cubit.dart';
+import 'ui/home_screen.dart';
+import 'ui/login.dart';
 import 'ui/splash.dart';
 import 'utils/routes.dart';
 
@@ -22,17 +25,31 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MultiBlocProvider(
-      providers: [
-        BlocProvider(create: (context) => LoginCubit()),
-        BlocProvider(create: (context) => RegisterCubit())
-      ],
-      child: MaterialApp(
-        title: "Praktikum 9",
-        debugShowCheckedModeBanner: false,
-        navigatorKey: NAV_KEY,
-        onGenerateRoute: generateRoute,
-        home: SplashScreen(),
-      ),
-    );
+        providers: [
+          BlocProvider(create: (context) => LoginCubit()),
+          BlocProvider(create: (context) => RegisterCubit())
+        ],
+        child: MaterialApp(
+          title: "Praktikum 9",
+          debugShowCheckedModeBanner: false,
+          navigatorKey: NAV_KEY,
+          onGenerateRoute: generateRoute,
+          home: StreamBuilder<User?>(
+            stream: FirebaseAuth.instance.authStateChanges(),
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return const CircularProgressIndicator();
+              } else if (snapshot.hasData) {
+                return const HomeScreen();
+              } else if (snapshot.hasError) {
+                return const Center(
+                  child: Text('Something went wrong'),
+                );
+              } else {
+                return LoginScreen();
+              }
+            },
+          ),
+        ));
   }
 }
