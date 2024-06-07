f48f80a33913a4df74176207751470436d557ffe
septyaanp <septyaandiniputri@gmail.com>
2024-06-07 08:23:58 +0700

    Update home_screen.dart

--- firebase_auth (Pertemuan 9)/lib/ui/home_screen.dart
+++ firebase_auth (Pertemuan 9)/lib/ui/home_screen.dart
@@ -1,39 +1,43 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:pertemuan9/ui/login.dart';
+import 'package:flutter/widgets.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
-class HomeScreen extends StatelessWidget {
+import 'login.dart';
+
+class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
 
+class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              "HOME SCREEN",
-              style: TextStyle(
-                  fontSize: 40,
-                  fontWeight: FontWeight.bold,
-                  color: Color(0xff3D4DE0)),
-            ),
-            ElevatedButton(
-              onPressed: () async {
-                FirebaseAuth.instance
-                    .signOut()
-                    .then((value) => Navigator.pushAndRemoveUntil(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => const LoginScreen(),
-                        ),
-                        (Route) => false));
-              },
-              child: const Text('Keluar'),
-            )
-          ],
-        ),
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        title: const Text('Note'),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.logout),
+            onPressed: () async {
+              GoogleSignIn().signOut();
+              FirebaseAuth.instance
+                  .signOut()
+                  .then((value) => Navigator.pushAndRemoveUntil(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => const LoginScreen(),
+                      ),
+                      (route) => false));
+            },
+          )
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: const Icon(Icons.add),
       ),
     );
   }
